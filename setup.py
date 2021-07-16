110c0c9c6523ef2deb041d10318e0de774b4cf28
Martin Mueller <damadden88@googlemail.com>
2021-07-16 09:15:15 +0200

    use public ip

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Setup script for docker-build-ami
 from setuptools import setup, find_packages
 
 # VERSION MUST be defined on line 10
-VERSION = '0.7.4'
+VERSION = '0.7.5'
 
 with open('requirements.txt') as f:
     requires = f.read().splitlines()
@@ -68,7 +68,7 @@ setup(
     install_requires=requires,
     tests_require=test_deps,
     extras_require=extras,
-    python_requires=">=3.6",
+    python_requires=">=2.7",
 
     entry_points={
         "console_scripts": [
